d39742f009703fd14d354188d8b6bc78b665f3d6
Eudald <eudald.correig@innovamat.com>
2023-08-26 09:08:48 +0200

    add wandb and move checks to utils.py

--- utils.py
+++ utils.py
@@ -1,5 +1,6 @@
 # check missing values
 import torch
+import wandb
 
 
 def check_for_missing_values(x, step):
@@ -10,3 +11,32 @@ def check_for_missing_values(x, step):
 def nan_to_unknown(x):
     # Replace the string "nan" with "Unknown"
     return x.replace("nan", "Unknown")
+
+
+def check_model_parameters(model, epoch):
+    # NOT IN USE AT THE MOMENT
+    check_for_missing_values(model, epoch)
+    # Clip gradients to avoid exploding gradients
+    nn.utils.clip_grad_norm_(combined_model.parameters(), 1)
+    # Clip parameters to avoid exploding parameters
+    for p in combined_model.parameters():
+        p.data.clamp_(-1, 1)
+
+
+def log_training_images(images, labels, outputs):
+    num_images_to_log = 5
+    for i in range(num_images_to_log):
+        # Convert image and label to numpy for visualization
+        image = images[i].cpu().numpy().transpose(1, 2, 0)
+        label = labels[i].cpu().numpy()
+        predicted_label = torch.argmax(outputs[i]).cpu().numpy()
+
+        # Log the image and label using wandb.Image and wandb.log
+        wandb.log(
+            {
+                "Training Image": wandb.Image(
+                    image,
+                    caption=f"True Label: {label}, Predicted Label: {predicted_label}",
+                )
+            }
+        )

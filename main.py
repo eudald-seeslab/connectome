eaf92ad4ecee632b0951c485acf8226e0df3827a
Eudald <eudald.correig@innovamat.com>
2023-09-08 14:02:20 +0200

    move main run to main.py and deprecate jupyter notebook major refactor to improve coding practices save weber function plots to dir with models

--- main.py
+++ main.py
@@ -1,65 +1,134 @@
-# DEPRECATED
-
+import pandas as pd
 from torch import nn, optim, device, cuda
 import torch
+
 import yaml
-from tqdm import trange
+from tqdm.auto import tqdm, trange
+
+import wandb
+from torch.utils.tensorboard import SummaryWriter
 
 from data_parser import adj_matrix, nodes
-from image_parser import train_loader
-from utils import check_for_missing_values
+from image_parser import train_loader, test_loader, debug_loader
+from run_functions import run_test_epoch, run_train_epoch
+from utils import plot_weber_fraction, print_run_details, handle_log_configs
 
-from network_models import CombinedModel
+from models import CombinedModel
+from model_config_manager import ModelConfigManager
+from model_manager import ModelManager
 
 config = yaml.safe_load(open("config.yml"))
-epochs = config["EPOCHS"]
+DEBUG = config["DEBUG"]
+epochs = config["EPOCHS"] if not DEBUG else 2
+RETINA_MODEL = config["RETINA_MODEL"]
+images_fraction = config["IMAGES_FRACTION"]
+continue_training = config["CONTINUE_TRAINING"]
+saved_model_path = config["SAVED_MODEL_PATH"]
+model_name = config["SAVED_MODEL_NAME"]
+save_every = config["SAVE_EVERY"]
+connectome_layer_number = config["CONNECTOME_LAYER_NUMBER"]
+wb = config["WANDB"]
+plot_weber = config["PLOT_WEBER_FRACTION"]
 
-problems = False
+if DEBUG and continue_training:
+    raise ValueError("Can't continue training in DEBUG mode")
 
-# Initialize the combined model
-combined_model = CombinedModel(adj_matrix, neurons=nodes)
+loader = debug_loader if DEBUG else train_loader
 
-# TODO: install cuda
+logger = handle_log_configs(DEBUG)
+
 dev = device("cuda" if cuda.is_available() else "cpu")
+if dev.type == "cpu":
+    logger.warning("WARNING: Running on CPU, so it might be slow")
+
+# Create the ModelConfigManager and load configurations from YAML files
+config_manager = ModelConfigManager(config)
+
+# Get a specific configuration by model name
+config_manager.set_model_config(RETINA_MODEL)
+
+combined_model = CombinedModel(
+    adj_matrix, neurons=nodes, model_config=config_manager.current_model_config
+)
+
+# Saving and loading manager
+model_manager = ModelManager(config, save_dir="models", clean_previous=True)
+
+if continue_training:
+    # If we want to continue training a saved model
+    model_manager.load_model(combined_model, saved_model_path)
+
+# Model details
 combined_model = combined_model.to(dev)
-
-# Specify the loss function and the optimizer
 criterion = nn.NLLLoss()
-optimizer = optim.Adam(combined_model.parameters(), lr=0.0001)
+optimizer = optim.Adam(combined_model.parameters(), lr=0.00001)
 
-for epoch in trange(epochs):
+# Logs
+# wandb sometimes screws up, so we might want to disable it (in config.yml)
+if wb:
+    wandb.init(project="connectome", config=config_manager.current_model_config)
+    _ = wandb.watch(combined_model, criterion, log="all")
+# I also want the model architecture
+tensorboard_writer = SummaryWriter()
+mock_image = torch.rand(1, 3, 512, 512).to(dev)
+tensorboard_writer.add_graph(combined_model, mock_image)
+
+print_run_details(config_manager, DEBUG, images_fraction, continue_training)
+
+# Training loop
+for epoch in trange(epochs, position=1, leave=True, desc="Epochs"):
     running_loss = 0
-    for images, labels in train_loader:
-        # Move images and labels to the device
-        images, labels = images.to(dev), labels.to(dev)
+    correct_predictions = 0
 
-        # Checks
-        if torch.isnan(images).any():
-            raise Exception("NaN in images")
-        if torch.isnan(labels).any():
-            raise Exception("NaN in labels")
+    for images, labels in tqdm(loader, position=0, leave=True, desc="Batches"):
+        running_loss += run_train_epoch(
+            combined_model, criterion, optimizer, images, labels, epoch, dev
+        )
 
-        # Forward pass
-        outputs = combined_model(images)
+    logger.info(f"Epoch {epoch+1}/{epochs}, Loss: {running_loss/len(train_loader)}")
 
-        # Compute the loss
-        loss = criterion(outputs, labels)
+    # Save model
+    if (epoch + 1) % save_every == 0:
+        model_manager.save_model(combined_model, epoch)
 
-        # Backward pass and optimize
-        optimizer.zero_grad()
-        loss.backward()
+# Close logs
+if wb:
+    wandb.finish()
+tensorboard_writer.close()
 
-        if problems:
-            # check for missing values in model parameters
-            check_for_missing_values(combined_model, epoch)
-            # Clip gradients to avoid exploding gradients
-            nn.utils.clip_grad_norm_(combined_model.parameters(), 1)
-            # Clip parameters to avoid exploding parameters
-            for p in combined_model.parameters():
-                p.data.clamp_(-1, 1)
+# Clean up intermediate models
+model_manager.clean_previous_runs()
 
-        optimizer.step()
+# Testing
+correct = 0
+total = 0
+test_results_df = pd.DataFrame(
+    columns=["Image", "Real Label", "Predicted Label", "Correct Prediction"]
+)
 
-        running_loss += loss.item()
 
-    print(f"Epoch {epoch+1}/{epochs}, Loss: {running_loss/len(train_loader)}")
+def get_image_names(i, _test_loader):
+    batch_image_names = [
+        a[0]
+        for a in _test_loader.dataset.dataset.samples[
+                 i * _test_loader.batch_size: (i + 1) * _test_loader.batch_size
+                 ]
+    ]
+    return batch_image_names, i + 1
+
+
+with torch.no_grad():
+    j = 0
+    for images, labels in tqdm(test_loader):
+        # Get the correct image names
+        image_names, j = get_image_names(j, test_loader)
+        correct, total, batch_df = run_test_epoch(
+            combined_model, images, labels, image_names, total, correct, dev
+        )
+        # Append the batch DataFrame to the list
+        test_results_df = pd.concat([test_results_df, batch_df], ignore_index=True)
+
+logger.info(f"Accuracy on the {total} test images: {100 * correct / total}%")
+
+if plot_weber:
+    plot_weber_fraction(test_results_df, model_manager.model_dir)
--- utils.py
+++ utils.py
@@ -3,9 +3,9 @@ import torch
 import wandb
 import torch.nn as nn
 import logging
-
-
-logger = logging.getLogger("training_log")
+import matplotlib.pyplot as plt
+import seaborn as sns
+import os
 
 
 def check_for_missing_values(x, step):
@@ -47,4 +47,48 @@ def log_training_images(images, labels, outputs):
                 }
             )
         except FileNotFoundError:
+            logger = logging.getLogger("training_log")
             logger.warning(f"Could not log training image")
+
+
+def plot_weber_fraction(test_results_df, save_dir):
+    # Calculate the percentage of correct answers for each Weber ratio
+    test_results_df['yellow'] = test_results_df['Image'].apply(lambda x: x.split('_')[1])
+    test_results_df['blue'] = test_results_df['Image'].apply(lambda x: x.split('_')[2])
+    test_results_df['weber_ratio'] = test_results_df.apply(
+        lambda row: max(int(row['yellow']), int(row['blue'])) / min(int(row['yellow']), int(row['blue'])), axis=1)
+    correct_percentage = test_results_df.groupby('weber_ratio')['Correct Prediction'].mean() * 100
+    # Plot
+    plt.figure(figsize=(10, 6))
+    sns.barplot(x=correct_percentage.index, y=correct_percentage.values)
+    plt.xlabel('Weber Ratio')
+    plt.ylabel('Percentage of Correct Answers')
+    plt.title('Percentage of Correct Answers for Each Weber Ratio')
+    plt.xticks(rotation=45)
+    plt.tight_layout()
+    # Save to model directory
+    plt.savefig(os.path.join(save_dir, 'weber_fraction_plot.png'))
+
+
+def print_run_details(config_manager, debug, images_fraction, continue_training):
+    logger = logging.getLogger("training_log")
+    config_manager.output_model_details()
+    if debug:
+        logger.warning("WARNING: Running on DEBUG mode, so using 10% of the images")
+    elif images_fraction < 1:
+        logger.warning(f"WARNING: Using {images_fraction * 100}% of the images")
+    if continue_training:
+        logger.warning("Warning: I'm training and already trained model")
+
+
+def handle_log_configs(debug):
+
+    logging.basicConfig(
+        filename="training_log.log",
+        level=logging.DEBUG if debug else logging.INFO,
+        format="%(asctime)s - %(levelname)s - %(message)s"
+    )
+    logger = logging.getLogger("training_log")
+    logger.addHandler(logging.StreamHandler())
+
+    return logger

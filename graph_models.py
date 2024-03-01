ccdc62801f30d29a42d4444f974cadbb3a3c059a
ecorreig <eudald@correig.net>
2024-03-01 11:41:45 +0100

    first working version of the connectome GNN

--- graph_models.py
+++ graph_models.py
@@ -3,27 +3,29 @@ from torch.nn import functional as F
 from torch_geometric.nn import GCNConv, global_mean_pool
 
 
+DROPOUT = 0.0
+
+
 class GNNModel(torch.nn.Module):
-    def __init__(self, num_node_features, num_classes):
+    def __init__(self, num_node_features, decision_making_vector):
         super(GNNModel, self).__init__()
-        self.conv1 = GCNConv(num_node_features, 16)
-        self.conv2 = GCNConv(
-            16, 32
-        )  # Use an intermediary feature size greater than num_classes
-        self.fc = torch.nn.Linear(
-            32, num_classes
-        )  # Fully connected layer to get to num_classes
+        self.conv = GCNConv(num_node_features, 1)
+        self.register_buffer("decision_making_vector", decision_making_vector)
 
     def forward(self, data):
         x, edge_index, batch = data.x, data.edge_index, data.batch
-        x = F.relu(self.conv1(x, edge_index))
-        x = F.dropout(x, training=self.training)
-        x = self.conv2(x, edge_index)
+        x = self.conv(x, edge_index)
+        x = F.relu(x)
+        x = F.dropout(x, training=self.training, p=DROPOUT)
 
-        # Global pooling
-        x = global_mean_pool(x, batch)  # Pool node features to the graph level
+        # Apply the decision-making mask
+        batch_size = batch.max().item() + 1
+        decision_mask = (
+            self.decision_making_vector.unsqueeze(0).repeat(batch_size, 1).view(-1, 1)
+        )
+        x = x * decision_mask
 
-        # Apply a final classifier
-        x = self.fc(x)
+        # Global pooling and final classifier
+        x = global_mean_pool(x, batch)
 
         return F.log_softmax(x, dim=1)

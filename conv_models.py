35b2550c12862b0a5a95fed587dc37737e87f22d
ecorreig <eudald@correig.net>
2024-04-02 16:32:04 +0200

    Add normalization channels to the conv models First model actually learning something

--- conv_models.py
+++ conv_models.py
@@ -4,26 +4,38 @@ import torch.nn.functional as F
 
 
 class AlternativeCNN(nn.Module):
-    def __init__(self):
+    def __init__(self, **kwargs):
         super(AlternativeCNN, self).__init__()
+
+        out_channels_1 = kwargs["out_channels_1"] if "out_channels_1" in kwargs else 16
+        out_channels_2 = kwargs["out_channels_2"] if "out_channels_2" in kwargs else 32
+        out_channels_3 = kwargs["out_channels_3"] if "out_channels_3" in kwargs else 64
+
         # Convolutional layers
-        self.conv1 = nn.Conv2d(1, 16, kernel_size=3, stride=1, padding=1)
-        self.conv2 = nn.Conv2d(16, 32, kernel_size=3, stride=1, padding=1)
-        self.conv3 = nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1)
+        self.conv1 = nn.Conv2d(1, out_channels_1, kernel_size=3, stride=1, padding=1)
+        self.conv1_bn = nn.BatchNorm2d(out_channels_1)
+        self.conv2 = nn.Conv2d(
+            out_channels_1, out_channels_2, kernel_size=3, stride=1, padding=1
+        )
+        self.conv2_bn = nn.BatchNorm2d(out_channels_2)
+        self.conv3 = nn.Conv2d(
+            out_channels_2, out_channels_3, kernel_size=3, stride=1, padding=1
+        )
+        self.conv3_bn = nn.BatchNorm2d(out_channels_3)
 
         # Adaptive pooling layer to reduce the spatial dimensions to 1x1 at the end
         self.adap_pool = nn.AdaptiveAvgPool2d((1, 1))
 
         # Fully connected layer for binary classification
-        self.fc = nn.Linear(64, 1)
+        self.fc = nn.Linear(out_channels_3, 1)
 
     def forward(self, x):
         # Apply convolutional layers with ReLU and max pooling
-        x = F.relu(self.conv1(x))
+        x = F.relu(self.conv1_bn(self.conv1(x)))
         x = F.max_pool2d(x, kernel_size=2, stride=2)  # Reducing to 256x256
-        x = F.relu(self.conv2(x))
+        x = F.relu(self.conv2_bn(self.conv2(x)))
         x = F.max_pool2d(x, kernel_size=2, stride=2)  # Reducing to 128x128
-        x = F.relu(self.conv3(x))
+        x = F.relu(self.conv3_bn(self.conv3(x)))
         x = F.max_pool2d(x, kernel_size=2, stride=2)  # Reducing to 64x64
 
         # Adaptive pooling to make the output size independent of the input size

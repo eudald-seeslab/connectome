0600962366acf157d99d1f1b896c633947b9464f
ecorreig <eudald@correig.net>
2024-05-08 08:39:12 +0200

    fix bug improve methods change naming, from "val" to "test"

--- create_input_images/shapes_creator.py
+++ create_input_images/shapes_creator.py
@@ -20,7 +20,7 @@ class ShapesGenerator:
     Attributes:
         shape (str): The shape of the geometric objects.
         train_num (int): The number of training images to generate.
-        val_num (int): The number of validation images to generate.
+        test_num (int): The number of test images to generate.
         min_radius (int): The minimum radius of the shapes.
         max_radius (int): The maximum radius of the shapes.
         jitter (bool): Whether to add jitter to the shapes.
@@ -31,23 +31,26 @@ class ShapesGenerator:
     background_colour = "#808080"
     boundary_width = 5
     colours = {colour_1: "#fffe04", colour_2: "#0003f9"}
+    img_paths = {}
 
-    def __init__(self, shape, train_num, val_num, min_radius, max_radius, jitter):
-        img_dir = f"images/{shape}_{min_radius}_{max_radius}_{'jitter' if jitter else ''}"
-        self.train_dir = os.path.join(img_dir, "train")
-        self.val_dir = os.path.join(img_dir, "val")
+    def __init__(self, shape, train_num, test_num, min_radius, max_radius, jitter):
+        self.img_dir = (
+            f"images/{shape}_{min_radius}_{max_radius}{'_jitter' if jitter else ''}"
+        )
+
         self.shape = shape
         self.train_num = train_num
-        self.val_num = val_num
+        self.test_num = test_num
         self.min_radius = min_radius
         self.max_radius = max_radius
         self.jitter = jitter
 
     def create_dirs(self):
         """Creates necessary directories to store generated images."""
-        for col in [self.colour_1, self.colour_2]:
-            os.makedirs(os.path.join(self.train_dir, col), exist_ok=True)
-            os.makedirs(os.path.join(self.val_dir, col), exist_ok=True)
+        for t in ["train", "test"]:
+            self.img_paths[f"{t}_1"] = os.path.join(self.img_dir, t, self.colour_1)
+            self.img_paths[f"{t}_2"] = os.path.join(self.img_dir, t, self.colour_2)
+        [os.makedirs(p, exist_ok=True) for p in self.img_paths.values()]
 
     def get_vertices(self, shape: str, center: tuple, radius: int) -> list:
         """Calculate vertices of the shapes based on the given parameters."""
@@ -92,9 +95,7 @@ class ShapesGenerator:
         else:
             raise ValueError(f"Shape {shape} not implemented.")
 
-    def draw_shape(
-        self, shape: str, radius: int, colour: str, iteration: int, jitter: bool = False, path=None
-    ):
+    def draw_shape(self, shape: str, radius: int, colour: str, jitter: bool = False):
         """Draws a single shape on an image and saves it to the appropriate directory."""
         pixels_x, pixels_y = 512, 512
         image = Image.new("RGB", (pixels_x, pixels_y), color=self.background_colour)
@@ -107,49 +108,55 @@ class ShapesGenerator:
         center = (int(pixels_x / 2) + dist_x, int(pixels_y / 2) + dist_y)
 
         vertices = self.get_vertices(shape, center, radius)
-        draw.polygon(vertices, fill=self.colours[colour])
+        if shape == "circle":
+            draw.ellipse(vertices, fill=self.colours[colour])
+        else:
+            draw.polygon(vertices, fill=self.colours[colour])
 
+        return image, int(np.sqrt(dist_x**2 + dist_y**2))
+
+    def save_image(self, image, radius, dist_from_center, it, path):
         file_path = os.path.join(
             path,
-            f"{shape}_{radius}_{int(np.sqrt(dist_x ** 2 + dist_y ** 2))}_{iteration}.png",
+            f"{self.shape}_{radius}_{dist_from_center}_{it}.png",
         )
         image.save(file_path)
 
     def generate_images(self):
-        """Generate sets of training and validation images."""
+        """Generate sets of training and test images."""
         min_radius, max_radius = self.min_radius, self.max_radius
         shape = self.shape
         jitter = self.jitter
 
         self.create_dirs()
-        train_1 = os.path.join(self.train_dir, self.colour_1)
-        train_2 = os.path.join(self.train_dir, self.colour_2)
-        val_1 = os.path.join(self.val_dir, self.colour_1)
-        val_2 = os.path.join(self.val_dir, self.colour_2)
+        radius_range = max_radius - min_radius
         print(
-            f"Creating images for {shape} with {self.train_num} training images and {self.val_num} validation images"
+            f"Creating images for {shape} with {radius_range * self.train_num} "
+            f"training images and {radius_range * self.test_num} test images."
         )
 
         for i in tqdm(range(self.train_num)):
             for r in range(min_radius, max_radius):
-                self.draw_shape(shape, r, self.colour_1, i, jitter, train_1)
-                self.draw_shape(shape, r, self.colour_2, i, jitter, train_2)
+                image, dist = self.draw_shape(shape, r, self.colour_1, jitter)
+                self.save_image(image, r, dist, i, self.img_paths["train_1"])
+                image, dist = self.draw_shape(shape, r, self.colour_2, jitter)
+                self.save_image(image, r, dist, i, self.img_paths["train_2"])
 
-        for i in tqdm(range(self.val_num)):
+        for i in tqdm(range(self.test_num)):
             for r in range(min_radius, max_radius):
-                self.draw_shape(shape, r, self.colour_1, i, jitter, val_1)
-                self.draw_shape(shape, r, self.colour_2, i, jitter, val_2)
-
-        return train_1, train_2, val_1, val_2
+                image, dist = self.draw_shape(shape, r, self.colour_1, jitter)
+                self.save_image(image, r, dist, i, self.img_paths["test_1"])
+                image, dist = self.draw_shape(shape, r, self.colour_2, jitter)
+                self.save_image(image, r, dist, i, self.img_paths["test_2"])
 
 
 if __name__ == "__main__":
     img_gen = ShapesGenerator(
         shape=config.SHAPE,
         train_num=config.TRAIN_NUM,
-        val_num=config.VAL_NUM,
+        test_num=config.TEST_NUM,
         min_radius=config.MIN_RADIUS,
         max_radius=config.MAX_RADIUS,
         jitter=config.JITTER,
     )
-    train_dir_1, train_dir_2, test_dir_1, test_dir_2 = img_gen.generate_images()
+    img_gen.generate_images()

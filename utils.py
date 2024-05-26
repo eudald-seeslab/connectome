f2b2cf94d3f234c3f3c00a109f037e704f6de7c7
ecorreig <eudald@correig.net>
2024-05-26 12:01:26 +0200

    Refactor error handling in plot_results function

--- utils.py
+++ utils.py
@@ -27,8 +27,9 @@ def plot_results(results_, plot_types):
                 plots.append(plot_accuracy_per_value(results_.copy(), "distance"))
             elif plot_type == "contingency":
                 plots.append(plot_contingency_table(results_.copy()))
-    except Exception as e:
-        print(f"Error plotting results: {e}")
+    except Exception:
+        error = traceback.format_exc()
+        print(f"Error plotting results: {error}")
 
     return plots
 
